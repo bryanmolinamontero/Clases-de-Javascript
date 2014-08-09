7e1ad7ab6e7c6292a11311b07920ca92c16cc342
Bryan Molina <bryanux@hotmail.com>
2014-08-09 12:15:58 -0500

    in de js, alert y comentarios

---
+++ Clase1/codigo.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Bryan Molina on 9/08/14.
+ */
+
+        // alert: función
+        // ():parametros de la función
+        // "": cadenas de texto
+        alert("Hola mamá ya sé programar!!!");
+        alert(5*20);
+        alert("10"+1);
